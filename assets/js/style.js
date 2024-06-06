dad2c1aeb724fd818ac17829b9faff0025300ea8
Yavorin <159899546+Yavorin@users.noreply.github.com>
2024-06-06 13:38:40 +0300

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,7 @@
+function Suma() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+    var sum = num1 + num2;
+    document.getElementById('result').value = sum;
+    alert("Результат: " + sum);
+  }
